14e0cbe23084cd1a1515501115d9ee60f54163b2
Revoj-a <revojjoever@gmail.com>
2025-09-19 22:26:34 +0800

    creating backend

---
+++ src/services/reminder.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export interface Reminder {
+  userId?: number;
+  id: number;
+  title: string;
+  completed?: boolean;
+}
+
+class ReminderService {
+  http = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com/",
+  });
+
+  async getReminders() {
+    const response = await this.http.get<Reminder[]>("/todos");
+    return response.data;
+  }
+
+  async addReminder(title: string) {
+    const response = await this.http.post<Reminder>("/todos", { title });
+    return response.data;
+  }
+
+  async removeReminder(id: number) {
+    const response = await this.http.delete("/todos/" + id);
+    return response.data;
+  }
+}
+
+const reminderService = new ReminderService();
+export default reminderService;
